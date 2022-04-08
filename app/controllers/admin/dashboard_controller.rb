c486c96996b0941a2775ff564807dcce9bd899f4
Phelan Faubert <phelanfaubert@gmail.com>
2022-04-08 02:21:28 +0000

    Added an admin dashboard page to show count of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD']
 
   def show
-    @product_count = Product.all.count
+    @num_products = Product.count
+    @num_categories = Category.count
   end
 end
