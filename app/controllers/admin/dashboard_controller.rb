e3a03f130c4cad02b2d6b0d41ce401b771c593be
Phelan Faubert <phelanfaubert@gmail.com>
2022-04-13 01:31:18 +0000

    Removed files

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD']
+
   def show
+    @num_products = Product.count
+    @num_categories = Category.count
   end
 end
